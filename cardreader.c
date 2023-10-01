ab5264dcb44a1c7bc1d616a880c01998fd500500
shaf22Raptor <146633449+shaf22Raptor@users.noreply.github.com>
2023-10-01 22:27:40 +1100

    Update cardreader.c

--- cardreader.c
+++ cardreader.c
@@ -8,6 +8,14 @@
 #include <unistd.h>
 #include <string.h>
 
+int testing() {
+/*
+sdhjflhjdasf
+    szdfjadfkjasdfljkasdf
+    */
+}
+
+
 // Struct used for card reader shared memory as specified
 typedef struct {
     char scanned[16];
