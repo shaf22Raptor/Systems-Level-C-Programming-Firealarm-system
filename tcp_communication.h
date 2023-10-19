80a3d64cbbf7588bf4203b1a9983f92e7856fd70
shaf22raptor <suddin1230@gmail.com>
2023-10-20 09:44:39 +1000

    added tcp.h file

---
+++ tcp_communication.h
@@ -0,0 +1,5 @@
+void configureServerAddress(struct sockaddr_in serverAddr, const char server_ip, int server_port);
+int createSocket();
+int establishConnection(int socket, struct sockaddr_in serverAddr, char *programName);
+int sendData(int socket, char *data);
+int receiveData(int socket, char *buffer);
