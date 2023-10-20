d3335738840f6efdbbe959d2c10d3de7261f196f
shaf22raptor <suddin1230@gmail.com>
2023-10-20 20:45:41 +1000

    bullshit bullshit bullshit

--- callpoint.c
+++ callpoint.c
@@ -14,7 +14,10 @@
 #include "udp_communication.h"
 
 // Message for emergency Datagram
-char header[4] = "FIRE";
+struct Data {
+    char header[4]; // {F,I,R,E}
+};
+
 
 typedef struct {
     char status; // '-' for inactive, '*' for active
@@ -24,24 +27,24 @@ typedef struct {
 
 int main(int argc, char **argv) 
 {
-    printf("\nentered main function\n");
+    struct Data fire;
+    strcpy(fire.header, "FIRE");
+
     // see if enough arguments were supplied for this program
     if (argc!=5) {
         fprintf(stderr, "usage: {resend delay (in microseconds)} {shared memory path} {shared memory offset} {fire alarm unit address:port}");
         exit(1);
     }
-    printf("\n checked supplied arguments\n");
+
     // intialise parameters for system by converting from char[] to int when necessary
     int resendDelay = atoi(argv[1]);
     const char *shm_path = argv[2];
     off_t shm_offset = (off_t)atoi(argv[3]);
     const char *firealarm_address_port = argv[4];
-    printf("\ncollected command line arguments\n");
 
     // Isolate port number from {ipAddress : port number}
     const char *portString= strstr(firealarm_address_port , ":");
     int portNumber = atoi(portString + 1);
-    printf("\ngot portnumber\n");
 
     /*********************************************
     Code to connect to share memory with simulator
@@ -49,14 +52,14 @@ int main(int argc, char **argv)
 
     // initialise shm
     int shm_fd = shm_open(shm_path, O_RDWR, 0);
-    printf("\nopen shared memory\n");
+
 
     // handle failed shm_open
     if (shm_fd == -1) {
         perror("shm_open()");
         exit(1);
     }
-    printf("\nshared memory fine\n");
+
     // Obtain statistics related to shm. Intended to find size of shm.
     struct stat shm_stat;
 
@@ -64,59 +67,59 @@ int main(int argc, char **argv)
         perror("fstat()");
         exit(1);
     }
-    printf("\nstat check fine\n");
+
     // mmap 
     char *shm = mmap(NULL, shm_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    printf("\nmmap performed\n");
     if (shm == MAP_FAILED) {
         perror("mmap()");
         exit(1);
     }
-    printf("\nmmap fine\n");
 
     // cast memory offset onto card_reader
     shm_callpoint *shared = (shm_callpoint *)(shm + shm_offset);
-    printf("\n shared identifier made\n");
     
     // Initialise UDP connection to fire alarm unit
+    /****************************************
+     * code to connect to fire alarm
+    ****************************************/
+
+    // Create UDP socket
+    int udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);   // Create socket for client and corresponding error handling
+    if (udp_sockfd == -1) { 
+        perror("\nsocket()\n");
+        return 1;
+    }
+    // Define server address and port
+    struct sockaddr_in firealarmAddr;
+    //configureServerAddress(&firealarmAddr, "127.0.0.1", firealarm_port);
+    firealarmAddr.sin_family = AF_INET;
+    firealarmAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    firealarmAddr.sin_port = htons(portNumber);
 
     // mutex lock for normal operation
     pthread_mutex_lock(&shared->mutex);
-
+    
     for(;;) {
-        printf("\nentered infinite for loop\n");
-        printf("\nstatus of callpoint should be written below\n");
-        printf("\nstatus of callpoint is %c\n", shared->status);
-        if (shared->status == '-') {
-            printf("\nreceived %s as message\n", shared->status);
-            //for(;;) {
+        if (shared->status == '*') {
+            printf("\n status is *\n");
+            for(;;) {
+                printf("\n entered next infinite for loop\n");
+                
                 /********************
                 SEND EMERGENCY ALARM
                 //******************/
+                printf("\nconfigured networking\n");
 
+                if (sendto(udp_sockfd, &fire, sizeof(fire), 0, (struct sockaddr *) &firealarmAddr, sizeof(firealarmAddr)) == -1) {
+                    perror("sendto failed");
+                    exit(1);
+                }
 
-                /****************************************
-                 * code to connect to fire alarm
-                ****************************************/
+                printf("\n it should have sent\n");
 
-                // Create UDP socket
-            /*    int udp_sockfd = createSocket();
-                printf("\ncreated socket\n");
-                // Define server address and port
-                struct sockaddr_in firealarmAddr;
-                //configureServerAddress(&firealarmAddr, "127.0.0.1", firealarm_port);
-                firealarmAddr.sin_family = AF_INET;
-                firealarmAddr.sin_port = htons(portNumber);
-                firealarmAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-                printf("\nconfigured connection settings\n");
-
-                printf("\nEntered 2nd infinite for loop\n");
-                int emergencyAlarm = sendData(udp_sockfd, header, &firealarmAddr);
-                printf("\nsent fire alarm message\n");
-                close(udp_sockfd);
                 usleep(resendDelay);
-                */
-            //}
+                
+            }
         }
         pthread_cond_wait(&shared->status, &shared->mutex);
     }
@@ -147,6 +150,8 @@ int main(int argc, char **argv)
         perror("munmap()");
     }
 
+
+    close(udp_sockfd);
     close(shm_fd);
 
     return 0;
