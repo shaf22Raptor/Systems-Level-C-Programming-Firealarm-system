01f76fc368f93ac0890033eac639cdd8fe5a365c
shaf22raptor <suddin1230@gmail.com>
2023-10-21 11:25:58 +1000

    final code for callpoint. UDP not included yet

---
+++ callpoint.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "udp_communication.h"
+
+// Message for emergency Datagram
+struct Data {
+    char header[4]; // {F,I,R,E}
+};
+
+
+typedef struct {
+    char status; // '-' for inactive, '*' for active
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} shm_callpoint;
+
+int main(int argc, char **argv) 
+{
+    struct Data fire;
+    strcpy(fire.header, "FIRE");
+
+    // see if enough arguments were supplied for this program
+    if (argc!=5) {
+        fprintf(stderr, "usage: {resend delay (in microseconds)} {shared memory path} {shared memory offset} {fire alarm unit address:port}");
+        exit(1);
+    }
+
+    // intialise parameters for system by converting from char[] to int when necessary
+    int resendDelay = atoi(argv[1]);
+    const char *shm_path = argv[2];
+    off_t shm_offset = (off_t)atoi(argv[3]);
+    const char *firealarm_address_port = argv[4];
+
+    // Isolate port number from {ipAddress : port number}
+    const char *portString= strstr(firealarm_address_port , ":");
+    int portNumber = atoi(portString + 1);
+
+    /*********************************************
+    Code to connect to share memory with simulator
+    *********************************************/
+
+    // initialise shm
+    int shm_fd = shm_open(shm_path, O_RDWR, 0);
+
+
+    // handle failed shm_open
+    if (shm_fd == -1) {
+        perror("shm_open()");
+        exit(1);
+    }
+
+    // Obtain statistics related to shm. Intended to find size of shm.
+    struct stat shm_stat;
+
+    if (fstat(shm_fd, &shm_stat) == -1) {
+        perror("fstat()");
+        exit(1);
+    }
+
+    // mmap 
+    char *shm = mmap(NULL, shm_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shm == MAP_FAILED) {
+        perror("mmap()");
+        exit(1);
+    }
+
+    // cast memory offset onto card_reader
+    shm_callpoint *shared = (shm_callpoint *)(shm + shm_offset);
+    
+    // Initialise UDP connection to fire alarm unit
+    /****************************************
+     * code to connect to fire alarm
+    ****************************************/
+
+    // Create UDP socket
+    int udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);   // Create socket for client and corresponding error handling
+    if (udp_sockfd == -1) { 
+        perror("\nsocket()\n");
+        return 1;
+    }
+    // Define server address and port
+    struct sockaddr_in firealarmAddr;
+    //configureServerAddress(&firealarmAddr, "127.0.0.1", firealarm_port);
+    firealarmAddr.sin_family = AF_INET;
+    firealarmAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    firealarmAddr.sin_port = htons(portNumber);
+
+    // mutex lock for normal operation
+    pthread_mutex_lock(&shared->mutex);
+    
+    for(;;) {
+        if (shared->status == '*') {
+            for(;;) {
+                
+                /********************
+                SEND EMERGENCY ALARM
+                //******************/
+
+                if (sendto(udp_sockfd, &fire, sizeof(fire), 0, (struct sockaddr *) &firealarmAddr, sizeof(firealarmAddr)) == -1) {
+                    perror("sendto failed");
+                    exit(1);
+                }
+
+                usleep(resendDelay);
+                
+            }
+        }
+        pthread_cond_wait(&shared->cond, &shared->mutex);
+    }
+    pthread_mutex_unlock(&shared->mutex);
+    
+    //general cleanup with error handling
+    if (shm_unlink(shm_path) == -1) {
+        perror("shm_unlink()");
+        exit(1);
+    }   
+
+    if(pthread_mutex_destroy(&shared->mutex) !=0) {
+        perror("pthread_mutex_destroy()");
+        exit(1);
+    }
+
+    if(pthread_cond_destroy(&shared->status) != 0) {
+        perror("pthread_cond_destroy");
+        exit(1);
+    }
+
+    if(pthread_cond_destroy(&shared->status) != 0) {
+        perror("pthread_cond_destroy");
+        exit(1);
+    }
+    
+    if (munmap(shm, shm_stat.st_size) == -1) {
+        perror("munmap()");
+    }
+
+
+    close(udp_sockfd);
+    close(shm_fd);
+
+    return 0;
+}
