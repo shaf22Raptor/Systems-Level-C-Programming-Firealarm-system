8d514f969560d8379d7d0cad8e55d80c212d7d08
Ronil Rathod <ronilrathod07@gmail.com>
2023-10-26 20:18:05 +1000

    This is makeing me crazy, udp is so annoying

--- firealarm.c
+++ firealarm.c
@@ -30,6 +30,27 @@ typedef struct {
     in_port_t door_port;
 } door_datagram;
 
+
+// Fire emergency datagram
+typedef struct {
+    char header[4]; // {'F', 'I', 'R', 'E'}
+} fire_datagram;
+
+// Temperature sensor controller datagrams
+struct addr_entry {
+  struct in_addr sensor_addr;
+  in_port_t sensor_port;
+};
+
+struct datagram_format {
+  char header[4]; // {'T', 'E', 'M', 'P'}
+  struct timeval timestamp;
+  float temperature;
+  uint16_t id;
+  uint8_t address_count;
+  struct addr_entry address_list[50];
+};
+
 // Door confirmation datagram structure
 typedef struct {
     char header[4]; // {'D', 'R', 'E', 'G'}
@@ -48,8 +69,10 @@ int detection_count = 0;
 
 // Main function
 int main(int argc, char **argv) {
-    if (argc != 8) {
-        fprintf(stderr, "Usage: firealarm {address:port} {temperature threshold} {min detections} {detection period (in microseconds)} {shared memory path} {shared memory offset} {overseer address:port}\n");
+    printf("Program started\n");
+
+    if (argc != 9) {
+        fprintf(stderr, "Usage: firealarm {address:port} {temperature threshold} {min detections} {detection period (in microseconds)} {reserved argument} {shared memory path} {shared memory offset} {overseer address:port}\n");
         return 1;
     }
 
@@ -58,9 +81,24 @@ int main(int argc, char **argv) {
     int temp_threshold = atoi(argv[2]);
     int min_detections = atoi(argv[3]);
     int detection_period = atoi(argv[4]);
-    char *shm_path = argv[5];
-    int shm_offset = atoi(argv[6]);
-    char *overseer_addr_port = argv[7];
+    char *shm_path = argv[6]; 
+    int shm_offset = atoi(argv[7]); 
+    char *overseer_addr_port = argv[8];
+    int udp_sockfd; // Socket descriptor
+    struct sockaddr_in myaddr; // server address
+    char *udp_ip = strtok(addr_port, ":");
+    char *udp_port_str = strtok(NULL, ":");
+    int udp_port = atoi(udp_port_str);
+    printf("Variables initialized from arguments\n"); // debug message
+
+    // if (!udp_ip || !udp_port_str) {
+    //     fprintf(stderr, "Error: address should be in the format ip:port\n");
+    //     exit(EXIT_FAILURE);
+    // }
+    // if (udp_port == 0) {
+    //     fprintf(stderr, "Error: Invalid port number\n");
+    //     exit(EXIT_FAILURE);
+    // }
 
     /* Socket setup for the door controller's server */
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);   /* Create a socket for communication */
@@ -70,6 +108,7 @@ int main(int argc, char **argv) {
     servaddr.sin_addr.s_addr = inet_addr(token);
     token = strtok(NULL, ":");
     servaddr.sin_port = htons(atoi(token));         /* Assign port to this socket */
+    printf("Socket created \n"); // debug message
 
     /* Binding the socket to the server address */
     if (bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
@@ -77,6 +116,7 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
     listen(sockfd, 10);
+    printf("Socket bound to address and listening\n"); // debug message
 
     /* Shared memory initialization */
     int shm_fd = shm_open(shm_path, O_RDWR, 0);
@@ -100,76 +140,41 @@ int main(int argc, char **argv) {
 
     shm_alarm *shared = (shm_alarm *)(shm + shm_offset);  /* Pointer to the shared structure */
     shared->alarm = '-';                               /* Initially, the door is considered closed */
-
-    // UDP socket setup for the fire alarm system's communication
-    int udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (udp_sockfd < 0) {
-        perror("Cannot create socket");
-        return EXIT_FAILURE;
-    }
-
-    // Create a separate sockaddr_in structure for UDP.
+    printf("Shared memory initialized\n"); // debug message
+    
+    /* Setup for the UDP socket */
     struct sockaddr_in udp_servaddr;
     memset(&udp_servaddr, 0, sizeof(udp_servaddr));
     udp_servaddr.sin_family = AF_INET;
+    udp_servaddr.sin_addr.s_addr = inet_addr(udp_ip);  // IP address from the command line
+    udp_servaddr.sin_port = htons(udp_port);  // Port number from the command line
 
-    // If the UDP server is supposed to bind to the same address and port, reset the tokenizer or use different variables.
-    char *udp_token; // No need for re-declaration, just use it.
-    udp_token = strtok(NULL, ":"); // Resetting to the first token.
-    udp_servaddr.sin_addr.s_addr = inet_addr(udp_token);
-    udp_token = strtok(NULL, ":");
-    udp_servaddr.sin_port = htons(atoi(udp_token));
-
-    // Bind to the UDP socket.
-    if (bind(udp_sockfd, (struct sockaddr*)&udp_servaddr, sizeof(udp_servaddr)) < 0) {
-        perror("UDP bind failed");
-        close(udp_sockfd);
+    // UDP socket creation for fire alarm system's communication
+    udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_sockfd < 0) {
+        perror("Cannot create UDP socket");
         return EXIT_FAILURE;
     }
 
-    // Setup TCP connection with overseer and send initialization message
-    /* Connect to overseer and send initialization message */
-    struct sockaddr_in overseer_addr;
-    memset(&overseer_addr, 0, sizeof(overseer_addr));
-    overseer_addr.sin_family = AF_INET;
-
-    /* Parsing overseer IP address and port */
-    char *overseer_ip = strtok(overseer_addr_port, ":");
-    char *overseer_port_str = strtok(NULL, ":");
-    if (!overseer_ip || !overseer_port_str) {
-        fprintf(stderr, "Error: Overseer address should be in the format ip:port\n");
+    // Binding the UDP socket to the local address and port
+    if (bind(udp_sockfd, (struct sockaddr*)&udp_servaddr, sizeof(udp_servaddr)) < 0) {
+        perror("bind failed for UDP socket");
         exit(EXIT_FAILURE);
     }
 
-    int overseer_port = atoi(overseer_port_str);
-    if (overseer_port == 0) {
-        fprintf(stderr, "Error: Invalid port number\n");
-        exit(EXIT_FAILURE);
+    printf("UDP socket created and bound to address\n"); // Debug message
+
+    char buffer[BUFFER_SIZE];  // Buffer for incoming data.
+    struct sockaddr_in remote_addr; // Address structure for the remote sender.
+    socklen_t addr_len = sizeof(remote_addr); // Variable for address length.
+
+    // Main loop
+    while (1) {
+ 
     }
 
-    if (inet_pton(AF_INET, overseer_ip, &overseer_addr.sin_addr) <= 0) {
-        fprintf(stderr, "Invalid overseer IP address\n");
-        exit(EXIT_FAILURE);
-    }
-    overseer_addr.sin_port = htons(overseer_port);
-
-    /* Communication setup with the overseer */
-    int overseer_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (overseer_sock < 0) {
-        perror("Cannot create socket");
-        exit(EXIT_FAILURE);
-    }
-
-    if (connect(overseer_sock, (struct sockaddr*)&overseer_addr, sizeof(overseer_addr)) < 0) {
-        perror("Connection to overseer failed");
-        close(overseer_sock);
-        exit(EXIT_FAILURE);
-    }
-
-    char init_msg[100];
-    snprintf(init_msg, sizeof(init_msg), "FIREALARM %s:%d HELLO#\n", inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port));
-    send(overseer_sock, init_msg, strlen(init_msg), 0);
-
-
+    close(sockfd); // TCP socket for door controller's server
+    close(udp_sockfd); // UDP socket for fire alarm system
+    // close(overseer_port); // TCP socket for communication with the overseer
     return 0;  // Successful exit
 }
