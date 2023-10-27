961e94ff18fd1be67909eeedf86c6489e35e7d7a
cab403 <cab403@cab403vm.qut.edu.au>
2023-10-27 13:09:25 +1000

    debugging firealarm

--- firealarm.c
+++ firealarm.c
@@ -34,12 +34,6 @@ typedef struct {
     in_port_t door_port;
 } door_datagram;
 
-
-// Fire emergency datagram
-typedef struct {
-    char header[4]; // {'F', 'I', 'R', 'E'}
-} fire_datagram;
-
 // Temperature sensor controller datagrams
 struct addr_entry {
   struct in_addr sensor_addr;
@@ -71,6 +65,12 @@ int door_count = 0;
 long long detection_timestamps[MAX_DETECTIONS];
 int detection_count = 0;
 
+// Fire emergency datagram
+char fire_emergency_header[] = {'F', 'I', 'R', 'E', '\0'}; 
+
+//Global shared
+shm_alarm *shared;
+
 // Main function
 int main(int argc, char **argv) {
     printf("Program started\n");
@@ -80,7 +80,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // Initialization of variables from arguments
+    // Initialisation of variables from arguments
     char *addr_port = argv[1];
     int temp_threshold = atoi(argv[2]);
     int min_detections = atoi(argv[3]);
@@ -88,7 +88,7 @@ int main(int argc, char **argv) {
     char *shm_path = argv[6]; 
     int shm_offset = atoi(argv[7]); 
     char *overseer_addr_port = argv[8];
-    struct sockaddr_in myaddr; // server address
+    // struct sockaddr_in myaddr; // server address
     printf("Variables initialized from arguments\n"); // debug message
 
     /* Socket setup for the door controller's server */
@@ -109,7 +109,7 @@ int main(int argc, char **argv) {
     listen(sockfd, 10);
     printf("Socket bound to address and listening\n"); // debug message
 
-    /* Shared memory initialization */
+    /* Shared memory initialisation */
     int shm_fd = shm_open(shm_path, O_RDWR, 0);
     if (shm_fd == -1) {
         perror("shm_open()");
@@ -129,8 +129,9 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    shm_alarm *shared = (shm_alarm *)(shm + shm_offset);  /* Pointer to the shared structure */
+    shared = (shm_alarm *)(shm + shm_offset);  /* Pointer to the shared structure */
     shared->alarm = '-';                               /* Initially, the door is considered closed */
+    
     printf("Shared memory initialized\n"); // debug message
     
     // Extracting UDP address and port from the command line
@@ -141,6 +142,11 @@ int main(int argc, char **argv) {
     char *udp_ip = strtok(udp_addr_port, ":");
     char *udp_port = strtok(NULL, ":");
     udp_servaddr.sin_port = htons(udp_port);  // Port number from the command line
+
+    if (inet_aton(udp_ip, &udp_servaddr.sin_addr) == 0) { // Convert from str to binary form
+        fprintf(stderr, "Invalid IP address format for UDP.\n");
+        exit(EXIT_FAILURE);
+    }
 
     // UDP socket creation
     int udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0);  // SOCK_DGRAM for UDP
@@ -159,7 +165,7 @@ int main(int argc, char **argv) {
 
     printf("UDP Socket bound to address and listening\n"); // debug message
 
-    /* Connect to overseer and send initialization message */
+    /* Connect to overseer and send initialisation message */
     struct sockaddr_in overseer_addr;
     memset(&overseer_addr, 0, sizeof(overseer_addr));
     overseer_addr.sin_family = AF_INET;
@@ -199,32 +205,86 @@ int main(int argc, char **argv) {
 
     printf("Connection to overseer done\n"); // debug message
 
-    // Prepare the initialization message
-    char init_message[BUFFER_SIZE];  // Buffer for the initialization message.
+    // Prepare the initialisation message
+    char init_message[BUFFER_SIZE];  // Buffer for the initialisation message.
     snprintf(init_message, sizeof(init_message), "FIREALARM %s HELLO#", addr_port);  // Construct the message with the local address and port.
 
-    // Send the initialization message to the overseer
+    // Send the initialisation message to the overseer
     ssize_t bytes_sent = send(overseer_sock, init_message, strlen(init_message), 0);
     if (bytes_sent < 0) {
-        perror("Failed to send initialization message to overseer");
+        perror("Failed to send initialisation message to overseer");
         close(overseer_sock);  // Close the overseer socket
         exit(EXIT_FAILURE);
     }
 
-    printf("Initialization message sent to overseer\n"); // debug message
+    printf("Initialisation message sent to overseer\n"); // debug message
 
-    char buffer[BUFFER_SIZE];  // Buffer for incoming data.
-    struct sockaddr_in remote_addr; // Address structure for the remote sender.
-    socklen_t addr_len = sizeof(remote_addr); // Variable for address length.
+    // char buffer[BUFFER_SIZE];  // Buffer for incoming data.
+    // struct sockaddr_in remote_addr; // Address structure for the remote sender.
+    // socklen_t addr_len = sizeof(remote_addr); // Variable for address length.
 
-    // Main loop
-    while (1) {
- 
-    }
+    // // Main loop
+    // while (1) {
+    //     memset(buffer, 0, BUFFER_SIZE);  // Clear the buffer.
+        
+    //     // Receive datagrams
+    //     ssize_t bytes_received = recvfrom(udp_sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&remote_addr, &addr_len);
+    //     if (bytes_received < 0) {
+    //         perror("Failed to receive UDP datagram");
+    //         continue;  // Skip the current iteration and wait for next datagram.
+    //     }
 
+    //     // Handle "FIRE" datagram
+    //     if (strncmp(buffer, fire_emergency_header, 4) == 0) {
+    //         handle_fire_alarm();
+    //         continue;
+    //     }
+        
+    //     // Handle "DOOR" datagram
+    //     if (strncmp(buffer, "DOOR", 4) == 0) {
+    //         if (door_count < MAX_DOORS) {
+    //             door_datagram *door_data = (door_datagram *)buffer;
+    //             door_addresses[door_count] = door_data->door_addr;
+    //             door_ports[door_count] = door_data->door_port;
+    //             door_count++;
+    //             send_door_confirmation(door_data->door_addr, door_data->door_port);
+    //         } else {
+    //             printf("Warning: Maximum number of doors reached. Cannot register more doors.\n");
+    //         }
+    //         continue;
+    //     }
+        
+    //     // Handle "TEMP" datagram
+    //     if (strncmp(buffer, "TEMP", 4) == 0) {
+    //         struct datagram_format *temp_data = (struct datagram_format *)buffer;
+            
+    //         // Check if the temperature exceeds the threshold
+    //         if (temp_data->temperature > temp_threshold) {
+    //             if (detection_count < MAX_DETECTIONS) {
+    //                 detection_timestamps[detection_count] = temp_data->timestamp.tv_sec * 1000000 + temp_data->timestamp.tv_usec;  // Convert to microseconds
+    //                 detection_count++;
+    //             }
+                
+    //             // Check the number of detections within the detection period
+    //             long long current_time = temp_data->timestamp.tv_sec * 1000000 + temp_data->timestamp.tv_usec;
+    //             int detections_within_period = 0;
+    //             for (int i = 0; i < detection_count; i++) {
+    //                 if (current_time - detection_timestamps[i] <= detection_period) {
+    //                     detections_within_period++;
+    //                 }
+    //             }
+                
+    //             if (detections_within_period >= min_detections) {
+    //                 handle_fire_alarm();
+    //             }
+    //         }
+    //         continue;
+    //     }
+    // }
+    munmap(shm, shm_stat.st_size);
     close(sockfd); // TCP socket for door controller's server
     close(udp_sockfd); // UDP socket for fire alarm system
-    close(overseer_sock) // TCP socket for communication with the overseer
+    close(overseer_sock); // TCP socket for communication with the overseer
     return 0;  // Successful exit
 }
 
@@ -234,7 +294,42 @@ void send_open_emergency_to_door(struct in_addr door_ip, in_port_t door_port) {
     // Connect to the door using the provided IP and port
     // Send the "OPEN_EMERG#" message
     // Close the socket
-    // You should handle any errors that might occur during this process
+    // and should handle any errors that might occur during this process
+
+    // Message to be sent to the door in case of an emergency
+    char emergency_message[] = "OPEN_EMERG#";
+    
+    // Create a new socket for TCP connection
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Error in socket creation");
+        return;
+    }
+
+    // Set up the destination address structure
+    struct sockaddr_in door_address;
+    memset(&door_address, 0, sizeof(door_address)); // Ensuring that the struct is empty
+    door_address.sin_family = AF_INET;              // Standard IPv4 address
+    door_address.sin_addr = door_ip;                // IP address comes from function argument
+    door_address.sin_port = door_port;              // Port number comes from function argument
+
+    // Establish a connection to the door system
+    if (connect(sock, (struct sockaddr*)&door_address, sizeof(door_address)) < 0) {
+        perror("Connection to the door system failed");
+        close(sock);
+        return;
+    }
+
+    // Send the emergency message to the door system
+    ssize_t bytes_sent = send(sock, emergency_message, strlen(emergency_message), 0);
+    if (bytes_sent < 0) {
+        perror("Failed to send message to door system");
+    } else if (bytes_sent < strlen(emergency_message)) {
+        printf("Warning: Not all bytes sent to door system\n");
+    }
+
+    // Close the socket after sending the data
+    close(sock);
 }
 
 // Additional function 2: Send a door confirmation UDP datagram as a reply
@@ -244,6 +339,38 @@ void send_door_confirmation(struct in_addr door_ip, in_port_t door_port) {
     // Send the confirmation message to the door system
     // Close the socket
     // Handle any errors appropriately
+
+    // Message to be sent as confirmation
+    door_confirmation confirmation;
+    memcpy(confirmation.header, "DREG", 4); // "DREG" signifies a door confirmation message
+    confirmation.door_addr = door_ip;
+    confirmation.door_port = door_port;
+
+    // Create a new socket for UDP connection
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("Error in socket creation");
+        return;
+    }
+
+    // Set up the destination address structure
+    struct sockaddr_in door_address;
+    memset(&door_address, 0, sizeof(door_address)); // Ensuring that the struct is empty
+    door_address.sin_family = AF_INET;              // Standard IPv4 address
+    door_address.sin_addr = door_ip;                // IP address comes from function argument
+    door_address.sin_port = door_port;              // Port number comes from function argument
+
+    // Send the confirmation message to the door system
+    ssize_t bytes_sent = sendto(sock, (const void *)&confirmation, sizeof(confirmation), 0, 
+                                (const struct sockaddr*)&door_address, sizeof(door_address));
+    if (bytes_sent < 0) {
+        perror("Failed to send confirmation to door system");
+    } else if (bytes_sent < sizeof(confirmation)) {
+        printf("Warning: Not all bytes sent to door system\n");
+    }
+
+    // Close the socket after sending the data
+    close(sock);
 }
 
 // Additional function 3: Set the alarm in your shared memory and follow the protocol
@@ -251,16 +378,66 @@ void handle_fire_alarm(void) {
     // Set the alarm in shared memory
     pthread_mutex_lock(&shared->mutex);
     shared->alarm = 'F'; // Indicate that the fire alarm is active
+    pthread_cond_broadcast(&shared->cond); // Notify any waiting threads about the alarm state
     pthread_mutex_unlock(&shared->mutex);
 
-    // Call Additional function 1 for every door in your list
+    // Notify every door about the emergency
     for (int i = 0; i < door_count; ++i) {
         send_open_emergency_to_door(door_addresses[i], door_ports[i]);
     }
 
-    // Start another infinite loop that only processes "DOOR" UDP datagrams
-    while (1) {
-        // Receive UDP datagrams
-        // If you receive "DOOR", call Additional function 1 and Additional function 2
+    // Prepare to receive "DOOR" UDP datagrams
+    int udp_sockfd;
+    struct sockaddr_in myaddr;
+    int recvlen;
+    unsigned char buf[BUFFER_SIZE]; // Buffer for receiving incoming messages.
+    
+    // Create a UDP socket
+    if ((udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("cannot create socket");
+        return;
     }
+
+    // Bind the socket to any valid IP address and a specific port
+    memset((char *)&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on any address
+    myaddr.sin_port = htons(0); // Listen on any free port, replace with specific if required
+
+    if (bind(udp_sockfd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+        perror("bind failed");
+        return;
+    }
+
+    // Continuous loop to process "DOOR" UDP datagrams
+    while (1) {
+        printf("Waiting on port (specify your port)...\n");
+        socklen_t addrlen = sizeof(myaddr); // length of addresses
+        recvlen = recvfrom(udp_sockfd, buf, BUFFER_SIZE, 0, (struct sockaddr *)&myaddr, &addrlen);
+
+        if (recvlen > 0) {
+            buf[recvlen] = 0; // null terminate the buffer
+            printf("Received message: \"%s\"\n", buf);
+
+            // Check if it's a "DOOR" message
+            door_datagram *door_data = (door_datagram *)buf;
+            if (memcmp(door_data->header, "DOOR", 4) == 0) { // "DOOR" header indicates a door message
+                // Respond to the door message, you may need to extract IP and port from the received message.
+                struct in_addr door_ip = door_data->door_addr; 
+                in_port_t door_port = door_data->door_port;
+
+                // Send emergency open command
+                send_open_emergency_to_door(door_ip, door_port);
+
+                // Send back a confirmation to the door system
+                send_door_confirmation(door_ip, door_port);
+            }
+        }
+        else if (recvlen < 0) {
+            perror("Error in recvfrom");
+        }
+    }
+
+    // Ideally, should never reach this point in an infinite loop
+    close(udp_sockfd); // Don't forget to close the socket at the end
 }
