4d5942fa5ef864404d709bbaef0fa1b44f872148
shaf22raptor <suddin1230@gmail.com>
2023-10-29 15:33:45 +1000

    fixed tcp.c

--- tcp_communication.c
+++ tcp_communication.c
@@ -8,53 +8,59 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#include "cardreader.c"
-
 int createSocket() {
     // Create a socket, set up the server address, and connect
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);   // Create socket for client and corresponding error handling
     if (sockfd == -1) { 
-        perror("socket()");
+        perror("\nsocket()\n");
         return 1;
     }
+
     return sockfd;
+
 }
 
-void configureServerAddressForClient(struct sockaddr_in addr, const char server_ip[]) {
+void configureServerAddressForClient(struct sockaddr_in addr, const char server_ip, int server_port) {
     memset(&addr, 0, sizeof(addr));
     if (inet_pton(AF_INET, server_ip, &addr.sin_addr) != 1) {
         perror("inet_pton()");
         return 1;
     }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(server_port);
 }
 
-void establishConnection(int socket, struct sockaddr_in serverAddr, int portNumber) {
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(portNumber);
-
+int establishConnection(int socket, struct sockaddr_in serverAddr, char *programName) {
     int connection_status = connect(socket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
     if (connection_status == -1) {
-        perror("connect()");
+        printf("Error: Connection to the server failed for %s\n", programName);
         return 1;
     }
+
+    return connection_status;
 }
 
-void sendData(int socket, char *data) {
+
+int sendData(int socket, char *data) {
     // Send data
     int sent = send(socket, data, strlen(data), 0);
     if (sent == -1) {
-        perror("send()");
+        printf("Error: Failed to send data\n");
         return 1;
     }
+
+    return sent;
 }
+
 
 int receiveData(int socket, char *buffer) {
     // Receive data
     int dataReceived = recv(socket, buffer, sizeof(buffer), 0);
     if (dataReceived == -1) {
-        perror("recv()");
+        printf("Error: receive data failed");
         return 1;
     }
+
     return dataReceived;
 }
